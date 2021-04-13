9c8dfdefa91795916c7da87945deb3bca88cdd58
carlosguerrero1982 <carlos.guerrero.rodriguez.dev@gmail.com>
2021-04-13 12:46:15 +0200

    neew

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header className="flex w-full p-5 justify-between">
+
+      <div className="flex space-x-4 items-center">
+        <p>About</p>
+        <p>Store</p>
+      </div>
+
+
+      <div className="flex space-x-4 items-center">
+        <p>Gmail</p>
+        <p>Images</p>
+
+
+      </div>
+
+      </header>
+
+  
+      
+  </div>
+  
+  )
+}
